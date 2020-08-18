6dfb2631823502525deab7c2443ed05b407697e6
xuemingming <xuemingming@zuowenzhitiao.com>
2020-08-18 16:11:28 +0800

    'TJ'

--- src/main/java/com/xmm/design/singleton/Singleton2.java
+++ src/main/java/com/xmm/design/singleton/Singleton2.java
@@ -4,15 +4,15 @@ package com.xmm.design.singleton;
  * 懒汉式单例模式
  */
 public class Singleton2 {
-    private static Singleton2 singleton2=null;
+    private static Singleton2 singleton2 = null;
     private Singleton2(){
 
     }
     public static Singleton2 getInstance(){
-        if(singleton2==null){
+        if (singleton2 == null){
             synchronized (Singleton2.class){
-                if(singleton2==null){
-                    singleton2=new Singleton2();
+                if(singleton2 == null){
+                    singleton2 = new Singleton2();
                     return singleton2;
                 }
             }
