dd079beeb3f54499d799e9d8a4a24d64b1e14d88
xuemingming <xuemingming@zuowenzhitiao.com>
2020-09-04 20:42:09 +0800

    '添加并发自测任务'

---
+++ src/main/java/com/xmm/concurrent/sync/SyncTest1.java
@@ -0,0 +1,38 @@
+package com.xmm.concurrent.sync;
+
+import lombok.extern.slf4j.Slf4j;
+import org.openjdk.jol.info.ClassLayout;
+
+/**
+ * 查看锁状态信息
+ */
+@Slf4j
+public class SyncTest1 {
+
+
+    public static void main(String[] args) throws InterruptedException {
+        log.info("lock加锁前，打印的对象的头信息为:");
+        Object o = new Object();
+        String lockHeadInfo = ClassLayout.parseInstance(o).toPrintable();
+        log.info(lockHeadInfo);
+
+        Thread.sleep(5000);
+        Object o2 = new Object();
+
+        log.info(ClassLayout.parseInstance(o2).toPrintable());
+        //调用synchronized之后，会进行锁撤销
+       // o2.hashCode();
+        log.info("开始使用synchronized进行上锁，观察当前对象的对象头是什么信息");
+        log.info(ClassLayout.parseInstance(o2).toPrintable());
+        Object o3 = new Object();
+        synchronized (o2){
+            log.info(ClassLayout.parseInstance(o2).toPrintable());
+        }
+
+    }
+}
+
+
+/**
+ * -XX:BiasedLockingStartupDelay=0 来取消延时加载偏向锁。
+ */
---
+++ src/main/java/com/xmm/concurrent/sync/SyncTest2.java
@@ -0,0 +1,50 @@
+package com.xmm.concurrent.sync;
+
+import org.openjdk.jol.info.ClassLayout;
+
+/**
+ * 轻量级锁测试
+ * 发生的情况是多个线程持有锁，但是没有发生竞争
+ */
+public class SyncTest2 {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(5000);
+        Object a = new Object();
+
+        Thread thread1 = new Thread(){
+            @Override
+            public void run() {
+                synchronized (a){
+                    System.out.println("thread1 locking");
+                    System.out.println(ClassLayout.parseInstance(a).toPrintable());
+                }
+                try {
+                    //thread1退出同步代码块，且没有死亡
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Thread thread2 = new Thread(){
+            @Override
+            public void run() {
+                synchronized (a){
+                    System.out.println("thread2 locking");
+                    System.out.println(ClassLayout.parseInstance(a).toPrintable());
+                }
+            }
+        };
+        thread1.start();
+
+        //让thread1执行完同步代码块中方法。
+        Thread.sleep(3000);
+        thread2.start();
+    }
+
+
+
+
+}
